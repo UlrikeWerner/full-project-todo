22884815e9a17a639e6da605bf915e9b4b45731e
UlrikeWerner <ulrike_werner@web.de>
2023-10-02 14:57:32 +0200

    set up pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
+import {Navigate, Route, Routes} from "react-router-dom";
+import Archive from "./pages/archive/Archive.tsx";
+import Backlog from "./pages/backlog/Backlog.tsx";
+import Overview from "./pages/overview/Overview.tsx";
+import WIP from "./pages/wip/WIP.tsx";
+
 export default function App() {
 
   return (
     <>
-
+      <Routes>
+          <Route path={"/"} element={<Overview />}/>
+          <Route path={"/backlog"} element={<Backlog />}/>
+          <Route path={"/working"} element={<WIP />}/>
+          <Route path={"/archive"} element={<Archive />}/>
+          <Route path={"/*"} element={<Navigate to={"/"}/>}/>
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/pages/archive/Archive.tsx
@@ -0,0 +1,8 @@
+export default function Archive() {
+
+    return (
+        <>
+            <h1>Archive</h1>
+        </>
+    )
+}
